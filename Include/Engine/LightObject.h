c5d99e1a2ed025f7bde88a476f63ae0347730235
enger <enger@spoko.pl>
2020-04-04 12:34:41 +0200

    libraries, moved definitions to .cpp files and cmake

--- Include/Engine/LightObject.h
+++ Include/Engine/LightObject.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "../GLES3/gl3.h"
-#include "../GLFW/glfw3.h"
-#include "../glm/glm.hpp"
-#include "../glm/gtc/matrix_transform.hpp"
-#include "../glm/gtc/type_ptr.hpp"
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #include <cmath>
-#include "../shader_s.h"
+#include "shader_s.h"
 #include "Camera.h"
 
 class point_light
@@ -15,86 +15,11 @@ class point_light
 public: 
    // lighting
     glm::vec3 lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
-    void create_light(Shader shader, unsigned int VBO, unsigned int VAO)
-    {
-         float vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-    };
-         
-
-        glGenVertexArrays(1, &VAO);
-        glBindVertexArray(VAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        // note that we update the lamp's position attribute's stride to reflect the updated buffer data
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-    }
-    void render_light(Shader shader, unsigned int VBO, unsigned int VAO)
-    {
-        // be sure to activate shader when setting uniforms/drawing objects
-        shader.use();
-        shader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-        shader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-        shader.setVec3("lightPos", lightPos);
-
-        
-
-        // world transformation
-        glm::mat4 model = glm::mat4(1.0f);
-        shader.setMat4("model", model);
-
-        // render the cube
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
-    void Delete_Object(unsigned int VBO, unsigned int VAO)
-    {
-        // optional: de-allocate all resources once they've outlived their purpose:
-        // ------------------------------------------------------------------------
-        glDeleteVertexArrays(1, &VAO);
-        glDeleteBuffers(1, &VBO);
-    }
+    void create_light(Shader shader, unsigned int VBO, unsigned int VAO);
+    void render_light(Shader shader, unsigned int VBO, unsigned int VAO);
+    
+    void Delete_Object(unsigned int VBO, unsigned int VAO);
+    
 private: 
     Camera camera;
 };
---
+++ src/Main.cpp
@@ -0,0 +1,125 @@
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cmath>
+#include "Engine/Camera.h"
+#include "Engine/Initialization.h"
+#include "Engine/CreateObject.h"
+#include "Engine/LightObject.h"
+
+
+// settings
+Init init_ref;
+Camera camera;
+Object object_ref;
+point_light light_ref;
+
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+int main()
+{
+    camera.SCR_HEIGHT = SCR_HEIGHT;
+    camera.SCR_WIDTH = SCR_WIDTH;
+    init_ref.create_window();
+    // build and compile our shader zprogram
+    // ------------------------------------
+    
+    Shader ourShader("assets/4.1.texture.vs", "assets/4.1.texture.fs");
+    Shader LightShader("assets/basic_lighting.vs", "assets/basic_lighting.fs");
+    
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    unsigned int VBO, VAO;
+    object_ref.Create_Object(ourShader,VBO, VAO);
+    unsigned int LightVAO;
+    //light_ref.create_light(LightShader, VBO, LightVAO);
+
+
+    // load and create a texture 
+    // -------------------------
+   
+
+
+    
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(init_ref.window))
+    {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput(init_ref.window, deltaTime);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+
+        // bind textures on corresponding texture units
+        
+
+        
+       
+       //light_ref.render_light(LightShader, VBO, LightVAO);
+       //camera.Camera_render(LightShader);
+       object_ref.Render_Object(ourShader,VBO, VAO);
+       camera.Camera_render(ourShader);
+       
+       
+       
+
+       
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(init_ref.window);
+        glfwPollEvents();
+    }
+
+    object_ref.Delete_Object(VBO, VAO);
+    light_ref.Delete_Object(VBO, LightVAO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window, float deltaTime)
+{
+    camera.processInput(window,deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.Process_Mouse_Scroll(yoffset);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    camera.Process_Mouse_Movement(xpos, ypos);
+}
